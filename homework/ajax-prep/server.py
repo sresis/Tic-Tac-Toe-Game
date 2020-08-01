40e06c18088e0a61a14e45c5a43d1b5e17234a71
Stephanie Resis <sresis@umich.edu>
2020-07-31 17:17:51 -0700

    initial commit

--- homework/ajax-prep/server.py
+++ homework/ajax-prep/server.py
@@ -15,8 +15,8 @@ def profile():
     """Return results from profile form."""
 
     fullname = request.form['name']
-    age = 'replace me'
-    occupation = 'replace me'
+    age = request.form['age']
+    occupation = request.form['occupation']
 
     # (pretend there's code to add the customer to the
     # database... we'll learn about that next week!)
